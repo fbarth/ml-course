4642444bb91f471068553e067130f441e0ccf3bb
Fabricio Barth <fbarth@br.ibm.com>
2018-09-05 21:38:27 -0300

    criacao do modelo preditivo para precos de imoveis

---
+++ scripts/02_02_preco_imoveis.R
@@ -0,0 +1,39 @@
+#
+# Script para criacao do modelo e avaliacao
+#
+
+load("data/20140917_imoveis_completo_tratado.rda")
+sort(table(imoveis$bairro), decreasing = TRUE)
+length(table(imoveis$bairro))
+
+set.seed(1234)
+ind <- sample(2, nrow(imoveis), replace = TRUE, prob = c(0.8, 0.2))
+train <- imoveis[ind == 1, ]
+test <- imoveis[ind == 2,]
+
+model <- lm(preco ~ ., data=train)
+summary(model)
+plot(model)
+
+# Function that returns Root Mean Squared Error
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+# Function that returns Mean Absolute Error
+mae <- function(error)
+{
+  mean(abs(error))
+}
+
+test$pred <- predict(model, test)
+rmse(test$preco - pred)
+mae(test$preco - pred)
+
+test$error <- abs(test$preco - test$pred)
+View(test)
+
+predTrain <- predict(model, train)
+rmse(train$preco - predTrain)
+mae(train$preco - predTrain)
