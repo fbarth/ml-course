13dc233ec1e334335650a13f7564134790e8910d
Fabricio Barth <fbarth@br.ibm.com>
2018-11-06 22:27:37 -0200

    Material sobre algoritmo de agrupamento

--- slides/Aula07-agrupamento/codigos/chunking/ex1.py
+++
@@ -1,5 +0,0 @@
-from nltk.tokenreader.tagged import ChunkedTaggedTokenReader
-chunked_string = "[ the/DT little/JJ cat/NN ] sat/VBD on/IN [ the/DT mat/NN ]"
-reader = ChunkedTaggedTokenReader(chunk_node='NP', SUBTOKENS='WORDS')
-sent_token = reader.read_token(chunked_string)
-print sent_token['TREE']
--- slides/Aula07-agrupamento/codigos/parsing/demos.py
+++
@@ -1,6 +0,0 @@
-# recursive descent parsing
-from nltk.draw.rdparser import demo
-demo()
-# shift-reduce parsing
-from nltk.draw.srparser import demo
-demo()
